ed022838cb07a897d0b008d34aa97e467db901b9
monica-wanjari <wanjari.monica1998@gmail.com>
2020-05-07 11:49:16 +0530

    Hosting code added

---
+++ build/precache-manifest.5e34b8b12b9e87584f7028b500ecb92d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13fd28d60ef18a47c512cb6987b4dab",
+    "url": "/panda/index.html"
+  },
+  {
+    "revision": "5986e5eadfd500b5618d",
+    "url": "/panda/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "006199475de39d16ab5b",
+    "url": "/panda/static/css/main.835df165.chunk.css"
+  },
+  {
+    "revision": "5986e5eadfd500b5618d",
+    "url": "/panda/static/js/2.21136737.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/panda/static/js/2.21136737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006199475de39d16ab5b",
+    "url": "/panda/static/js/main.53d26a64.chunk.js"
+  },
+  {
+    "revision": "e2e2700f89c46bc87c7d",
+    "url": "/panda/static/js/runtime-main.c2fd42eb.js"
+  }
+]);
